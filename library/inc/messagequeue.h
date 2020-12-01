ceaea0dedfde8f2bdcc04983a4491ea9bf907631
SanderWesseldijk <55689512+SanderWesseldijk@users.noreply.github.com>
2020-12-01 18:36:21 +0100

    .

--- library/inc/messagequeue.h
+++ library/inc/messagequeue.h
@@ -3,9 +3,19 @@
 #include <mutex>
 #include <optional>
 #include <string>
+#include <vector>
 
 class MessageQueue
 {
 public:
+    MessageQueue();
+    ~MessageQueue();
+    MessageQueue(const MessageQueue&);
+    MessageQueue operator=(const MessageQueue&);
+
+    std::optional<std::string> try_read();
+    void write(std::string msg);
 private:
+    std::list<std::string> messages;
+    std::mutex m;
 };
--- library/src/messagequeue.cpp
+++ library/src/messagequeue.cpp
@@ -4,3 +4,34 @@
 #include <thread>
 
 using namespace std::literals::chrono_literals;
+
+MessageQueue::MessageQueue()
+{
+    
+}
+
+MessageQueue::~MessageQueue()
+{
+
+}
+
+std::optional<std::string> MessageQueue::try_read()
+{
+    std::lock_guard<std::mutex>(*m);
+    if (messages.empty())
+    {
+        return std::nullopt;
+    }
+    else
+    {
+        auto result = std::move(messages.front());
+        messages.pop_front();
+        return result;
+    }
+}
+
+void MessageQueue::write(const std::string msg)
+{
+    std::lock_guard<std::mutex>(*m);
+    messages.push_back(std::move(msg));
+}
